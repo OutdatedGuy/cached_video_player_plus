4d36521ed3f3df2c41fe00b15995a107f008f56c
OutdatedGuy <everythingoutdated@gmail.com>
2024-04-11 02:29:56 +0530

    chore: updated plugin & example template to Flutter 3.19 (#15)

--- example/android/app/src/main/kotlin/rocks/outdatedguy/cached_video_player_plus_example/MainActivity.kt
+++ example/android/app/src/main/kotlin/rocks/outdatedguy/cached_video_player_plus_example/MainActivity.kt
@@ -2,5 +2,4 @@ package rocks.outdatedguy.cached_video_player_plus_example
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
-}
+class MainActivity: FlutterActivity()
